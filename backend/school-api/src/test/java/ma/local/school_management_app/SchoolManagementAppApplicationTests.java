122bf83f125b63f0cb4063a450dc4730ed4271a0
issam <issamsghiouare@gmail.com>
2026-04-22 13:43:03 +0100

    Fix CI/CD

--- backend/school-api/src/test/java/ma/local/school_management_app/SchoolManagementAppApplicationTests.java
+++ backend/school-api/src/test/java/ma/local/school_management_app/SchoolManagementAppApplicationTests.java
@@ -2,8 +2,10 @@ package ma.local.school_management_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SchoolManagementAppApplicationTests {
 
 	@Test
--- backend/src/main/java/ma/local/school_management_app/SchoolManagementAppApplication.java
+++ backend/src/main/java/ma/local/school_management_app/SchoolManagementAppApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@ActiveProfiles("test")
 public class SchoolManagementAppApplication {
 
 	public static void main(String[] args) {
