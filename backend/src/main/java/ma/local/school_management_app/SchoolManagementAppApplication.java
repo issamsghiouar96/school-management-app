5f5df0fea3cf8e3ffd967c1b3bca3049c7407cb6
issam <issamsghiouare@gmail.com>
2026-04-22 13:21:09 +0100

    Code refactoring

--- backend/src/main/java/ma/local/school_management_app/SchoolManagementAppApplication.java
+++ backend/src/main/java/ma/local/school_management_app/SchoolManagementAppApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ActiveProfiles("test")
 public class SchoolManagementAppApplication {
 
 	public static void main(String[] args) {
